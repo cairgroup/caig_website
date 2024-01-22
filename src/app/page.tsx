d4beb6faacbf5d0e0537e699b5d5498deb592072
Ron <ronnachum13@gmail.com>
2024-01-22 12:13:59 -0500

    Fix mobile r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-screen flex-col items-start justify-between p-12 overflow-hidden bg-matte-black-radial">
-        <div className="animate-fade-up flex flex-col items-start justify-start pt-24">
+    <main className="flex min-h-screen w-screen flex-col items-start justify-between p-12 overflow-hidden bg-matte-black-radial text-white">
+        <div className="animate-fade-up flex flex-col items-start justify-start sm:pt-2 md:pt-24">
           <Image src="/logo.png" alt="CAIRG" height={75} width={75} className="mb-2"/>
-          <h1 className='text-6xl mb-3'>Cambridge AI Reading Group</h1>
-          <h3 className='text-xl italic'>Mondays @ 7 PM</h3>
+          <h1 className='text-6xl mb-3 text-white'>Cambridge AI Reading Group</h1>
+          <h3 className='text-xl italic text-white'>Mondays @ 7 PM</h3>
         </div>
 
-        <div className="animate-fade-up flex flex-col items-end justify-start w-full">
+        <div className="animate-fade-up flex flex-col sm:items-center md:items-end justify-start w-full">
           <a target="_blakn" 
             href="mailto:ronnachum@college.harvard.edu?subject=Interest%20in%20Cambridge%20AI%20Reading%20Group&body=Your%20background%20working%20with%20AI%20and%20why%20you're%20interested%20in%20joining..." 
-            className="mb-3 w-1/3 inline-block px-6 py-3 text-white border-[1px] border-white bg-gray-900 rounded-lg hover:rounded-3xl hover:shadow-md transition-all ease-in-out 2s text-center"
+            className="mb-3 sm:w-3/4 md:w-1/3 inline-block px-6 py-3 text-white border-[1px] border-white bg-gray-900 rounded-lg hover:rounded-3xl hover:shadow-md transition-all ease-in-out 2s text-center"
           >
             <h3>Join the group!</h3>
           </a>
@@ -20,7 +20,7 @@ export default function Home() {
         </div>
 
         <div className="animate-fade-up flex flex-col items-center justify-start h-32 pb-36 w-full">
-          <h2 className='text-2xl mb-3'>Supported By</h2>
+          <h2 className='text-2xl mb-3 text-white'>Supported By</h2>
           <div className="flex flex-row items-center justify-center w-screen h-[100px]">
             <div className="flex flex-col items-center justify-center w-1/3 h-[100px]">
               <Image src="/EF_white.png" alt="EntrepreneurFirst" height={160} width={350}/>
