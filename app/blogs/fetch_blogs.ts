0c21e05a16b1d169a151d681a26cc1bf7618d56c
Eli <eliolcott@gmail.com>
2024-07-26 20:29:01 -0400

    Added specifics to an object

--- app/blogs/fetch_blogs.ts
+++ app/blogs/fetch_blogs.ts
@@ -16,22 +16,18 @@ export async function getStaticProps(): Promise<ReturnStatus> {
       );
 
       const header_lines = markdown.split('---')[1].split('\n');
-      console.log(`Header lines: ${header_lines}`);
-      const blog_metadata: any = {};
+      const blog_metadata: { [key: string]: any } = {};
       for (const header_line of header_lines) {
         const [json_title, json_info] = header_line.split(': ');
         blog_metadata[json_title] = json_info;
       }
 
       blog_metadata['slug'] = dir_name;
+      blog_metadata['published'] = blog_metadata['published'] === 'true';
 
-      return blog_metadata;
+      return blog_metadata as BlogMetadata;
     }).filter((blog_metadata) => {
-      if (blog_metadata['published'] === 'false') {
-        return false;
-      }
-
-      return true;
+      return blog_metadata['published'];
     });
 
     return {
@@ -44,55 +40,7 @@ export async function getStaticProps(): Promise<ReturnStatus> {
       status: 404,
       statusMessage: 'error',
       output: null,
-      message: error,
+      message: error.message,
     }
   }
 }
-
-
-// export async function getStaticPropsBlogs(): Promise<ReturnStatus> {
-//   try {
-//     const filePath = path.join('public', 'content', 'metadata.json');
-//     // const fileContents = fs.readFileSync(filePath, 'utf8');
-//     // const metadata = JSON.parse(fileContents);
-//     const file3 =
-//     const file2 = path.resolve(__dirname, '../../public/content/metadata.json');
-//     const file = path.join(process.cwd(), '/public/content/metadata.json');
-//     console.log(file);
-//     console.log(file2);
-//     // const blogsMetadata = fs.readFileSync(file, 'utf-8');
-//     const blogsMetadata = fs.readFileSync(file2, 'utf-8');
-//     console.log(blogsMetadata);
-//     const blogPosts: BlogMetadata[] = JSON.parse(blogsMetadata)
-
-//     return {
-//       status: 200,
-//       statusMessage: "success",
-//       output: blogPosts,
-//     };
-//   } catch (error) {
-//     console.error(error);
-//     return {
-//       status: 404,
-//       statusMessage: "error",
-//       output: null,
-//     };
-//   }
-// }
-
-// export async function getStaticProps({ params: { slug } }) {
-//   const markdownWithMeta = fs.readFileSync(
-//     path.join('posts', slug + '.md'),
-//     'utf-8'
-//   )
-
-//   const { data: frontmatter, content } = matter(markdownWithMeta)
-
-//   return {
-//     props: {
-//       frontmatter,
-//       slug,
-//       content,
-//     },
-//   }
-// }
