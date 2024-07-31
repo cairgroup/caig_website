d5e1411c1b029e30eb50a24d62656b50dc52b6f9
Eli <eliolcott@gmail.com>
2024-07-30 23:36:24 -0400

    Attempt 42

--- app/api/fetch_internal_files/route.ts
+++ app/api/fetch_internal_files/route.ts
@@ -15,7 +15,6 @@ export async function GET(req: Request): Promise<NextResponse> {
     const file = fs.readFileSync(
       path.join(process.cwd(), `content/${file_name}/${file_name}.md`), 'utf-8'
     );
-    throw new Error (`FAILED MISERABLY: ${file}`);
 
     return NextResponse.json({
       status: 200,
---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  experimental: {
+    outputFileTracingIncludes: {
+      'app/**/*.[jt]s': ['./content/**/*'],
+    },
+  },
+}
